4d1651966fda77794affd6f871da7c2283ba3213
Matthew Chupp <mattlchupp@gmail.com>
2015-03-20 13:55:42 -0500

    Initial Commit

---
+++ EasyToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyToDo
+//
+//  Created by Matthew Chupp on 3/20/15.
+//  Copyright (c) 2015 MattChupp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
